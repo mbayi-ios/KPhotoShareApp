57c89e7eb2e51711f0212a065180149e3bbee5b8
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-09-01 14:34:08 +0300

    - able to download images and save them and retrieve from file manager
    - able to download images and save them and retrieve from file manager

--- KPhotoShareApp/Services/ImageService.swift
+++ KPhotoShareApp/Services/ImageService.swift
@@ -3,26 +3,46 @@ import SwiftUI
 import Combine
 
 class ImageService {
-
     @Published var image: UIImage? = nil
 
     private var imageSubscription: AnyCancellable?
     private let post: PostModel
 
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "post_images"
+    private let imageName: String
+
     init(post: PostModel){
         self.post = post
+        self.imageName = post.id
         getPhoto()
     }
+
     private func getPhoto() {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName){
+            image = savedImage
+            print("Retrieved image from file Manager")
+        } else {
+            downloadPhoto()
+            print("Download image now")
+        }
+
+    }
+
+    private func downloadPhoto() {
         guard let url = URL(string: post.thumbnailURL) else { return }
 
         imageSubscription = NetworkingManager.download(url: url)
-            .tryMap({ (data) -> UIImage? in
+            .tryMap({(data) -> UIImage? in
                 return UIImage(data: data)
             })
-            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: {[weak self] (returnedImage) in
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: {[weak self](returnedImage) in
+                guard let self = self, let downloadedImage = returnedImage else { return }
+                self.image = downloadedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             })
+
     }
+
 }
