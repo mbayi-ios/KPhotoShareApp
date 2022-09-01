a25474e008f2fca92050f001dfa23186fa25d55c
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-09-01 13:34:32 +0300

    Initial Commit

---
+++ KPhotoShareApp/KPhotoShareAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KPhotoShareAppApp.swift
+//  KPhotoShareApp
+//
+//  Created by Amby on 01/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct KPhotoShareAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
