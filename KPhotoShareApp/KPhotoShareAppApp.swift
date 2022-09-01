f85e62b2635bac36808f5ccce7534d99c60777ab
mbayi-ios <91916741+mbayi-ios@users.noreply.github.com>
2022-09-01 14:17:09 +0300

    - added networking layer - refactor PostDataService to use Networking Layer
    - added networking layer
    - refactor PostDataService to use Networking Layer

--- KPhotoShareApp/KPhotoShareAppApp.swift
+++ KPhotoShareApp/KPhotoShareAppApp.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 @main
 struct KPhotoShareAppApp: App {
+    @StateObject private var vm = HomeViewModel()
     var body: some Scene {
         WindowGroup {
-            HomeView()
+            NavigationView {
+                HomeView()
+            }
+            .environmentObject(vm)
+
         }
     }
 }
--- KPhotoShareApp/Services/PostDataService.swift
+++ KPhotoShareApp/Services/PostDataService.swift
@@ -13,27 +13,12 @@ class PostDataService {
     private func getPosts() {
         guard let url = URL(string: "https://api1.kiliaro.com/shared/djlCbGusTJamg_ca4axEVw/media" ) else { return }
 
-        postSubscription = URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { (output) -> Data in
-                guard let response = output.response as? HTTPURLResponse,
-                      response.statusCode >= 200 && response.statusCode < 300 else {
-                          throw URLError(.badServerResponse)
-                      }
-                return output.data
-            }
-            .receive(on: DispatchQueue.main)
+        postSubscription = NetworkingManager.download(url: url)
             .decode(type: [PostModel].self, decoder: JSONDecoder())
-            .sink {(completion) in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { [weak self](returnedPosts) in
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: {[weak self] (returnedPosts) in
                 self?.allPosts = returnedPosts
                 self?.postSubscription?.cancel()
-            }
+            })
+
     }
 }
